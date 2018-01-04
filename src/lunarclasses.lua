0011882f6783a3b3878673f9cca36200472ddd3d
QwertygidQ <Qwertygid@gmail.com>
2018-01-05 02:08:09 +0300

    Added the first version

---
+++ src/lunarclasses.lua
@@ -0,0 +1,41 @@
+lunarclasses = {}
+
+local function search(field, parent_classes)    
+    for k, class in pairs(parent_classes) do
+        local in_class_field = class[field]
+        if in_class_field then
+            return in_class_field
+        end
+    end
+end
+
+function lunarclasses.create_class(parent_classes, constructor)
+    if parent_classes and type(parent_classes) ~= "table" then
+        error("Class: parent_classes should be a table")
+    elseif constructor and type(constructor) ~= "function" then
+        error("Class: constructor should be a function")
+    end
+    
+    parent_classes = parent_classes or {}
+    
+    class = {super=parent_classes, static={}}
+    setmetatable(class, {__index =
+                            function(table, field)
+                                local in_class_field = search(field, parent_classes)
+                                table[field] = in_class_field
+                                return in_class_field
+                            end
+                        })
+    class.__index = class
+    
+    constructor = constructor or
+        function(self, o)
+            o = o or {}
+            setmetatable(o, class)
+            return o
+        end
+    
+    class.new = constructor
+    
+    return class
+end
